52411e7b0d6a8b44fc5438d2973128f4f208e4dc
loosimin16 <sl913@duke.edu>
2024-10-16 23:36:14 -0400

    5

--- mylib/query.py
+++ mylib/query.py
@@ -19,7 +19,7 @@ def general_query(query):
     """runs a query a user inputs"""
     load_dotenv()
     server_h = os.getenv("SERVER_HOSTNAME")
-    access_token = os.getenv("DATABRICKS_TOKEN")
+    access_token = os.getenv("DATABRICKS_KEY")
     http_path = os.getenv("HTTP_PATH")
     with sql.connect(
         server_hostname=server_h,
